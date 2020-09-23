50061e1ef1c89f70c8a16928a0d0e939a0d1f649
Hamill <1544152390121002@mil>
2020-09-23 17:15:46 -0400

    Update url passing to async main, hack to workaround IA mesonet servers

--- RTMA_Pressure_async_download.py
+++ RTMA_Pressure_async_download.py
@@ -14,6 +14,7 @@ import asyncio
 import aiohttp
 import async_timeout
 
+
 async def download_coroutine(url, session, destination):
     with async_timeout.timeout(1200):
         async with session.get(url) as response:
@@ -30,7 +31,7 @@ async def download_coroutine(url, session, destination):
             return await response.release()
 
 async def main(loop, tmp, destination):
-
+       
     async with aiohttp.ClientSession() as session:
         tasks = [download_coroutine(url, session, destination) for url in tmp]
         return await asyncio.gather(*tasks)
@@ -44,15 +45,15 @@ if __name__ == '__main__':
     end = datetime(2019,1, 1, 0, 0)
     hour = timedelta(hours=1)
     destination = r"C:\Users\RDCRLDDH\Desktop\RTMA_Pressure_WY19"
-
+    
     #loop through and see if you already have the file locally
     date = start
     urls = []
     opath = []
     while date < end:
-        url = "http://mtarchive.geol.iastate.edu/{:04d}/{:02d}/{:02d}/grib2/ncep/RTMA/{:04d}{:02d}{:02d}{:02d}00_TMPK.grib2".format(
+        url = "http://mtarchive.geol.iastate.edu/{:04d}/{:02d}/{:02d}/grib2/ncep/RTMA/{:04d}{:02d}{:02d}{:02d}00_PRES.grib2".format(
             date.year, date.month, date.day, date.year, date.month, date.day, date.hour)
-
+        
         filename = url.split("/")[-1]
         if not os.path.isfile(destination + os.sep + filename):
             urls.append(url)
@@ -61,9 +62,9 @@ if __name__ == '__main__':
 
     #Split urls into chunks so you wont overwhelm IA mesonet with asyncronous downloads
     chunk_size = 50
-    chunked_urls = [urls[i * chunk_size:(i + 1) * chunk_size] for i in range((len(urls) + chunk_size - 1) // chunk_size )]
+    chunked_urls = [urls[i * chunk_size:(i + 1) * chunk_size] for i in range((len(urls) + chunk_size - 1) // chunk_size )] 
 
     for tmp in chunked_urls:
-        loop = asyncio.get_event_loop()
+        loop = asyncio.get_event_loop()        
         results = loop.run_until_complete(main(loop, tmp, destination))
         del loop, results
